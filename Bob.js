de6125a6cfb668cf972a8d446008233c5eb1068c
NandiniSinha17 <68430112+NandiniSinha17@users.noreply.github.com>
2020-08-25 16:59:05 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,27 @@
+class Bob {
+    constructor(x, y, radius) {
+        var options = {
+            'isStatic' : false,
+            'restitution' : 0.3,
+            'friction' : 0.5,
+            'density' : 1.2
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+
+    display(){
+       var pos = this.body.position;
+       var angle = this.body.angle;
+       push();
+       translate(pos.x, pos.y);
+       rotate(angle);
+       strokeWeight(0.5);
+       stroke("pink");
+       fill(254, 0, 253);
+       ellipseMode(CENTER);
+       ellipse(0, 0, this.radius*2);
+       pop();
+    }
+}
